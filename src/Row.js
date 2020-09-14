444e91bcacd0a6766c20dade7819f41e38a93a25
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-14 21:28:48 +0300

    Fetch movie data

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
 
 function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(fetchUrl);
+
+      setMovies(response.data.results);
+    }
+
+    fetchData();
+  }, [fetchUrl]);
   return (
     <div>
       <h2>{title}</h2>
