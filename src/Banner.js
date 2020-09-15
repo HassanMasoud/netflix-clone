dcdc40f6a7afa183b92080cb5e71874fe4e41d6c
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-15 21:44:39 +0300

    Create Banner component

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import "./Banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -20,7 +21,31 @@ function Banner() {
 
   console.log(movie);
 
-  return <header></header>;
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+        backgroundPosition: "top center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <p className="banner__description">{truncate(movie?.overview, 180)}</p>
+      </div>
+    </header>
+  );
 }
 
 export default Banner;
