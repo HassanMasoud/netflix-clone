f54ea6a983023bdc69817dc02a9a9d87afaab549
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-15 18:50:04 +0300

    Fetch banner movie

---
+++ src/Banner.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import requests from "./requests";
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        response.data.results[
+          Math.floor(Math.random() * response.data.results.length) - 1
+        ]
+      );
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
+  return <header></header>;
+}
+
+export default Banner;
